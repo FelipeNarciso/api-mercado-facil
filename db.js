562669f872a56cf1ab11be61c92ecb78553591c5
FelipeNarciso <luisfelipenb88@gmail.com>
2024-10-03 14:45:58 -0300

    update db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  database: 'railway',
-  user: 'root',
-  password:'tBOUKEhVymcFkYLQsnouWEDIkcqshEAa',
-  host:'junction.proxy.rlwy.net',
-  port: '35011'
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT
 })
 
 connection.connect((err) => {
